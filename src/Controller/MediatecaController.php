a7a030ba397522769c0a8a95437ad7dc79734dca
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-08-15 13:19:36 -0600

    Primer query a la base de datos

--- src/Controller/MediatecaController.php
+++ src/Controller/MediatecaController.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mediateca\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Messenger\MessengerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -11,20 +12,29 @@ class MediatecaController extends ControllerBase{
      * @var \Drupal\Core\Messenger\MessengerInterface
      */
     protected $messenger;
+    protected $connection;
 
-    public function __construct(MessengerInterface $messenger){
+    public function __construct(MessengerInterface $messenger, Connection $connection){
         $this->messenger = $messenger;
+        $this->connection = $connection;
     }
 
     public static function create(ContainerInterface $container)
     {
         return new static(
-            $container->get('messenger')
+            $container->get('messenger'),
+            $container->get('database')
         );
     }
 
     public function content(){
 
+        $result = $this->connection->select('node_field_data', 'n')
+            ->fields('n', ['nid', 'title'])
+            ->execute()
+            ->fetchAll();
+
+        dpm($result);
 
         $this->messenger->addMessage('Hola Mundo');
 
