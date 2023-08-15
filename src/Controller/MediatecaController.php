ed63a81ae8612d00de09d6e5221c6687c7a8001a
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-08-15 13:01:05 -0600

    Agrego servicio messenger

--- src/Controller/MediatecaController.php
+++ src/Controller/MediatecaController.php
@@ -5,11 +5,14 @@ use Drupal\Core\Controller\ControllerBase;
 
 class MediatecaController extends ControllerBase{
     public function content(){
-        $output = '';
-        $output .= '<p>Esta es la pagina de ayuda del modulo Escuela</p>';
-        return [
-            '#type' => 'markup',
-            '#markup' => $output,
+
+
+        $messenger = \Drupal::messenger();
+
+        $messenger->addMessage('Hola Mundo');
+        $output = [];
+        $output['primero'] = [
+            '#markup' => '<h1>Este es el primer parrafo</h1>',
         ];
     }
 }
